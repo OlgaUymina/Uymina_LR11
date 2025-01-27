aa33daa1d0bbef4e916accf39483212c97b4b948
OlgaUymina <tigrmail25@ya.ru>
2025-01-27 16:52:00 +0300

    create void main

--- Uymina_LR1_project.cpp
+++ Uymina_LR1_project.cpp
@@ -1,1 +1,20 @@
-//hghghgvh
+
+double Operacii()
+{
+  
+switch {
+    case "Ввести A":
+        break;
+    case "Ввести B":
+        break;
+    case "выполнить операцию “*” ":
+        break;
+    case "выполнить операцию “/” ":
+        break;
+    default:
+
+ }
+ // Please enter the number of the desired function or 0 if you want to close the app:
+ // разрабатывается Уйминой
+
+ }
